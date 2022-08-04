a0cf087df8e2405a2a2f14898e3f8e403115c6b8
atchrislee <110001612+atchrislee@users.noreply.github.com>
2022-08-04 19:49:16 -0400

    moved requests to bottom

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,5 @@
 import streamlit
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -23,3 +21,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #display table on page
 streamlit.dataframe(fruits_to_show)
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
